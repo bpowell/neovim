b509e1566b4f41634d09b26951b96f39f97ca531
Brandon Powell <bpowell29a@gmail.com>
2023-08-12 20:53:55 -0700

    feat: more plugins

--- init.lua
+++ init.lua
@@ -1,6 +1,40 @@
 vim.opt.number = true
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.keymap.set('i', 'jj', '<Esc>', {})
 
 require('plugins')
+
+-- LSP Configuration
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+require("mason-lspconfig").setup_handlers {
+    function (server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {}
+    end,
+}
+-- End LSP Configuration
+
+-- telescope Configuration
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+-- End telescope Configuration
+
+-- NVIM Tree Configuration
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+require("nvim-tree").setup({
+    view = {
+        width = 50,
+    },
+})
+vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', {})
+-- End NVIM Tree Configuration
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,6 +7,14 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
   use 'neovim/nvim-lspconfig'
+
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+
+  use 'nvim-tree/nvim-web-devicons'
+  use 'nvim-tree/nvim-tree.lua'
 
 end)
